81539b8571606d097ea144de54374ee8249c6091
BibatRalph <BibatRalph@gmail.com>
2023-04-11 03:25:02 +0800

    import UI job details

--- src/pages/Jobs/job-details.tsx
+++ src/pages/Jobs/job-details.tsx
@@ -1,6 +1,41 @@
-import React from 'react'
+import { Typography, Box, Stack } from "@mui/material";
+import { useDelete, useGetIdentity, useShow } from "@refinedev/core";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+    ChatBubble,
+    Delete,
+    Edit,
+    Phone,
+    Place,
+    Star,
+} from "@mui/icons-material";
+import { CustomButton } from "components";
 
 const jobDetails = () => {
+
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity({
+      v3LegacyAuthProviderCompatible: true,
+  });
+  const { queryResult } = useShow();
+  const { mutate } = useDelete();
+  const { id } = useParams();
+  
+  const { data, isLoading, isError } = queryResult;
+
+  const propertyDetails = data?.data ?? {};
+
+  if (isLoading) {
+      return <div>Loading...</div>;
+  }
+
+  if (isError) {
+      return <div>Something went wrong!</div>;
+  }
+
+  const isCurrentUser = user.email === propertyDetails.creator.email;
+
+
   return (
     <div>job-details</div>
   )
