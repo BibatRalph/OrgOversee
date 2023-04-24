6f5718f85df014ce18d01d28b98724fdc01308d6
BibatRalph <BibatRalph@gmail.com>
2023-04-24 23:47:41 +0800

    Create OFF UI

--- src/pages/TimeOffs/CreateOff.tsx
+++ src/pages/TimeOffs/CreateOff.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
-import { Box, Stack, Paper, Grid ,Typography } from "@mui/material";
+import { Box, Stack, Paper, Grid ,Typography, FormControl,   TextField, Button, } from "@mui/material";
 import { DatePicker, LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
 import { Dayjs } from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+//SUBMIT
+import { useGetIdentity } from "@refinedev/core";
+import { useForm } from "@refinedev/react-hook-form";
+import { FieldValues } from "react-hook-form";
 
 const CreateOff = () => {
-
+    //GET user info
+  const { data: user } = useGetIdentity({
+    v3LegacyAuthProviderCompatible: true,
+});
+const {
+  refineCore: { onFinish, formLoading },
+  register,
+  handleSubmit,
+} = useForm();
+const onFinishHandler = async (data: FieldValues) => {
+  await onFinish({
+      ...data,
+      email: user.email,
+  });
+};
   const [value, setValue] = React.useState<Dayjs | null>(null);
-  console.log(value);
+  console.log(typeof value);
+
   return (
     <>
     <Paper
@@ -35,6 +52,7 @@ sx={{
 <Typography variant="h5">
 Create Time-Off
 </Typography>
+
 </Stack>
 {/* CONTENT */}
 <Box
@@ -42,21 +60,68 @@ Create Time-Off
         sx={{
             display: "flex",
             flexWrap: "wrap",
-            gap: "20px",
-        }}
+            gap: "10px",
+        }}    
     >
-      <Stack>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-  
-<StaticDatePicker  value={value} onChange={(newValue) => setValue(newValue)} orientation="landscape" />
-
-</LocalizationProvider>
-
-      </Stack>
-  
-    
       
+<Stack >
+<LocalizationProvider dateAdapter={AdapterDayjs}> 
+<StaticDatePicker  value={value} onChange={(newValue) => setValue(newValue)} orientation="landscape" />
+</LocalizationProvider>  
+  </Stack>         
+
+      {/*INFo  */}
+      <Stack  width="100%" padding={2}>
+<Typography fontSize={16} color="#808191">
+*Employee Information
+</Typography>
+<FormControl>
+                        <TextField
+                            fullWidth
+                            required
+                            id="outlined-basic"
+                            color="primary"
+                            variant="standard"
+                            label="Time-Off Date"
+                            disabled
+                            {...register("jobTitle", { required: true })}
+                        />
+                        </FormControl>
+                        <FormControl>
+                        <TextField
+                            fullWidth
+                            required
+                            id="outlined-basic"
+                            color="primary"
+                            variant="standard"
+                            label="Name"
+                            disabled
+                            {...register("jobTitle", { required: true })}
+                        />
+                        </FormControl>
+                        <FormControl>
+                        <TextField
+                            fullWidth
+                            required
+                            id="outlined-basic"
+                            color="primary"
+                            variant="standard"
+                            label="ID"
+                            disabled
+                            {...register("jobTitle", { required: true })}
+                        />
+                        </FormControl>
+                       </Stack>
+
+    <Stack  
+    mt={3}
+    width="100%">
+      <Button>SUBMIT</Button>
+    </Stack>
+
+  
     </Box>
+ 
 </Grid>
 </Paper>
 </>
