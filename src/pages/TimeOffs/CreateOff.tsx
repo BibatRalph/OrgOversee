67fd26cd8ad153ef0f90202a840464e07f143d33
BibatRalph <BibatRalph@gmail.com>
2023-04-25 02:10:34 +0800

    create TimeOFF UI

--- src/pages/TimeOffs/CreateOff.tsx
+++ src/pages/TimeOffs/CreateOff.tsx
@@ -1,32 +1,54 @@
+import { useCreate, useGetIdentity, useShow } from "@refinedev/core";
+import { useNavigate } from "react-router-dom";
+import { CustomButton } from "components";
 import * as React from 'react';
-import { Box, Stack, Paper, Grid ,Typography, FormControl,   TextField, Button, } from "@mui/material";
-import { DatePicker, LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
+import { Box, Stack, Paper, Grid ,Typography} from "@mui/material";
+import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { Dayjs } from 'dayjs';
-//SUBMIT
-import { useGetIdentity } from "@refinedev/core";
-import { useForm } from "@refinedev/react-hook-form";
-import { FieldValues } from "react-hook-form";
 
 const CreateOff = () => {
     //GET user info
   const { data: user } = useGetIdentity({
     v3LegacyAuthProviderCompatible: true,
 });
-const {
-  refineCore: { onFinish, formLoading },
-  register,
-  handleSubmit,
-} = useForm();
-const onFinishHandler = async (data: FieldValues) => {
-  await onFinish({
-      ...data,
-      email: user.email,
-  });
-};
-  const [value, setValue] = React.useState<Dayjs | null>(null);
-  console.log(typeof value);
 
+const navigate = useNavigate();
+
+const [value, setValue] = React.useState<Dayjs | null>(null);
+
+const { queryResult } = useShow();
+  const { mutate } = useCreate();
+
+  const { data } = queryResult;
+
+  const offData = data?.data ?? {};
+
+  const handleCreateOff = () => {
+    const response = confirm(
+        "Are you sure you want to Apply for this Time-Off?",
+    );
+    if (response) {
+        mutate(
+            {
+                resource: "Timeoff",
+               
+                values: {
+                    // REQ
+                    date: value,
+                    email: user.email, //  email
+                    id: user._id, //  id 
+                    name: user.name, //  name
+                },
+            },
+            {
+                onSuccess: () => {
+                    navigate("/Timeoff");
+                },
+            },
+        );
+    }
+};
   return (
     <>
     <Paper
@@ -36,8 +58,10 @@ sx={{
   my: 0.5,
 }}
 > 
-
 <Grid item xs={16} md={12}  >
+
+
+                    
    {/* TOP BAR */}
          <Stack
                             display="flex"
@@ -52,76 +76,41 @@ sx={{
 <Typography variant="h5">
 Create Time-Off
 </Typography>
-
+<Stack>
+  *Select date before Submitting
+</Stack>
 </Stack>
 {/* CONTENT */}
-<Box
-        mt="20px"
-        sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "10px",
-        }}    
-    >
-      
+
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
 <Stack >
 <LocalizationProvider dateAdapter={AdapterDayjs}> 
 <StaticDatePicker  value={value} onChange={(newValue) => setValue(newValue)} orientation="landscape" />
 </LocalizationProvider>  
   </Stack>         
-
-      {/*INFo  */}
-      <Stack  width="100%" padding={2}>
-<Typography fontSize={16} color="#808191">
-*Employee Information
-</Typography>
-<FormControl>
-                        <TextField
-                            fullWidth
-                            required
-                            id="outlined-basic"
-                            color="primary"
-                            variant="standard"
-                            label="Time-Off Date"
-                            disabled
-                            {...register("jobTitle", { required: true })}
-                        />
-                        </FormControl>
-                        <FormControl>
-                        <TextField
-                            fullWidth
-                            required
-                            id="outlined-basic"
-                            color="primary"
-                            variant="standard"
-                            label="Name"
-                            disabled
-                            {...register("jobTitle", { required: true })}
-                        />
-                        </FormControl>
-                        <FormControl>
-                        <TextField
-                            fullWidth
-                            required
-                            id="outlined-basic"
-                            color="primary"
-                            variant="standard"
-                            label="ID"
-                            disabled
-                            {...register("jobTitle", { required: true })}
-                        />
-                        </FormControl>
-                       </Stack>
-
-    <Stack  
-    mt={3}
-    width="100%">
-      <Button>SUBMIT</Button>
-    </Stack>
-
-  
-    </Box>
- 
+                        <Stack
+                          justifyContent="center"
+                          alignItems="center" 
+                       mt="10px" marginBottom={3}>
+                        
+                        <CustomButton
+                         
+                         title="SUBMIT"
+                         backgroundColor="#67be23"
+                         color="#FCFCFC"
+                         fullWidth
+                         handleClick={() => {
+                              handleCreateOff();
+                         }}
+                     />
+                        </Stack>    
+                        </Stack>                      
+             
 </Grid>
 </Paper>
 </>
