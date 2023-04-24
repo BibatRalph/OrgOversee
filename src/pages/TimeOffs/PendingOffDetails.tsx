3f524571ff7d751dad2fcf391ca0bb255c882d49
BibatRalph <BibatRalph@gmail.com>
2023-04-24 19:22:51 +0800

    UI timeoffs box and grids

--- src/pages/TimeOffs/PendingOffDetails.tsx
+++ src/pages/TimeOffs/PendingOffDetails.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { Box, Stack, Paper, Grid ,Typography } from "@mui/material";
 
 const PendingOffDetails = () => {
   return (
-    <div>PendingOffDetails</div>
+    <>
+    <Paper
+sx={{
+  paddingX: { xs: 3, md: 1 },
+  paddingY: { xs: 2, md: 3 },
+  my: 0.5,
+}}
+> 
+
+<Grid item xs={16} md={12}  >
+   {/* TOP BAR */}
+         <Stack
+                            display="flex"
+                            justifyContent="space-between"
+                            alignItems="baseline"
+                            flexWrap="wrap"
+                            padding={3}
+                            direction="row"
+                            gap={2}
+                        >
+                     
+<Typography variant="h5">
+Pending Time-Off
+</Typography>
+</Stack>
+{/* CONTENT */}
+<Box
+        mt="20px"
+        sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "20px",
+        }}
+    >
+      
+      
+    </Box>
+</Grid>
+</Paper>
+</>
   )
 }
 
